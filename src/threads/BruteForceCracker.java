fd05a98eeffb2ca3a1d199b5c28190f40e2c9a13
amanddali <league@WTS20.attlocal.net>
2018-08-26 15:55:00 -0700

    threads

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -1,3 +1,5 @@
+package threads;
+
 // Below is a completed program that uses brute force to crack a 9-digit password. 
 // On average, the program takes about 0.125 seconds to find the password.
 // Your goal is to re-write the algorithm using Threads. Try to shorten the time it takes 
@@ -9,29 +11,55 @@
 import java.util.Random;
 
 public class BruteForceCracker {
-	static long code = (long)(new Random().nextDouble() * 1_000_000_000);
-	
+	static long code = (long) (new Random().nextDouble() * 1_000_000_000);
+
 	static long startTime;
 	static long endTime;
 	static float elapsedTime;
-	
+
 	public static void main(String[] args) {
 		System.out.println("Starting Brute Force Checker");
 		startTime = System.currentTimeMillis();
+
+		Thread t1 = new Thread(()->{
+			for (int i = 0; i < 25_000_000; i++) {
+				checkCode(i);
+		}	
+		});
 		
-		int ctr = 0;
-		while(!checkCode(ctr++));
+		Thread t2 = new Thread(()->{
+			for (int i = 250_000_000; i < 500_000_000; i++) {
+				checkCode(i);
+			}
+		});
+		
+		Thread t3 = new Thread(()->{
+			for (int i = 500_000_000; i < 750_000_000; i++) {
+				checkCode(i);
+			}
+		});
+		
+		Thread t4 = new Thread(()->{
+			for (int i = 750_000_000; i < 1_000_000_000; i++) {
+				checkCode(i);
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 		
 		endTime = System.currentTimeMillis();
-		elapsedTime = (float)(endTime - startTime);
+		elapsedTime = (float) (endTime - startTime);
 		elapsedTime /= 1000.f;
 		System.out.println("Total time taken: " + elapsedTime + " seconds");
 	}
 	
-	public static boolean checkCode(long p){
-		if(p == code){
+	public static boolean checkCode(long p) {
+		if (p == code) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,80 @@
 package threads;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
 
+		Robot kiki = new Robot(400, 550);
+		Robot fefe = new Robot(600, 550);
+		Robot kb = new Robot(800, 550);
+		Robot tinashe = new Robot(500, 700);
+		Robot tekashi = new Robot(700, 700);
+		int move = 2;
+
+		Thread r1 = new Thread(() -> {
+			kiki.setPenColor(Color.BLUE);
+			kiki.setPenWidth(3);
+			kiki.penDown();
+			kiki.setSpeed(10);
+			for (int i = 0; i < 360; i++) {
+				kiki.move(move);
+				kiki.turn(360 / 360);
+			}
+		});
+		
+		Thread r2 = new Thread(() -> {
+			fefe.setPenColor(Color.BLACK);
+			fefe.setPenWidth(3);
+			fefe.penDown();
+			fefe.setSpeed(10);
+			for (int i = 0; i < 360; i++) {
+				fefe.move(move);
+				fefe.turn(360 / 360);
+			}
+		});
+		
+		Thread r3 = new Thread(() -> {
+			kb.setPenColor(Color.RED);
+			kb.setPenWidth(3);
+			kb.penDown();
+			kb.setSpeed(10);
+			for (int i = 0; i < 360; i++) {
+				kb.move(move);
+				kb.turn(360 / 360);
+			}
+		});
+		
+		Thread r4 = new Thread(() -> {
+			tinashe.setPenColor(Color.YELLOW);
+			tinashe.setPenWidth(3);
+			tinashe.penDown();
+			tinashe.setSpeed(10);
+			for (int i = 0; i < 360; i++) {
+				tinashe.move(move);
+				tinashe.turn(360 / 360);
+			}
+		});
+		
+		Thread r5 = new Thread(() -> {
+			tekashi.setPenColor(Color.GREEN);
+			tekashi.setPenWidth(3);
+			tekashi.penDown();
+			tekashi.setSpeed(10);
+			for (int i = 0; i < 360; i++) {
+				tekashi.move(move);
+				tekashi.turn(360 / 360);
+			}
+		});
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		
+	}
 }
-
