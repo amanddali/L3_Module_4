0f57b617642cb4be2503ccec5c61cc2c4cf8b17b
League <league@WTS8.attlocal.net>
2018-09-02 15:42:09 -0700

    finished threads

--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -5,4 +5,26 @@ public class ThreadedReverseGreeting {
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
   //but you should structure your program such that the threads print their greetings in reverse order.
+	
+	public static void main(String[] args) {
+		Thread(1);
+	}
+	
+	public static void Thread(int x) {
+		if (x == 50) {
+			System.out.println("Hello from Thread 50");
+		}else {
+			Thread t = new Thread(() -> {
+				Thread(x+1);
+				System.out.println("Hello from Thread " + x);
+			});
+			t.start();
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
